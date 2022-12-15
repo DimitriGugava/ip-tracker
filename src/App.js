0bb508b895fb9bc62d6528ca042584cce1f520dd
Dima Gugava <dimitrigugavinho@gmail.com>
2022-12-15 14:08:07 +0400

    style: added header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import HeaderLayout from "./HeaderLayout/HeaderLayout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HeaderLayout />
+      <Map />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/HeaderLayout/HeaderLayout.jsx
@@ -0,0 +1,38 @@
+import "./HeaderLayout.css";
+import bluebackground from "../icons/bluebackground.svg";
+import arrow from "../icons/arrow.svg";
+
+const HeaderLayout = () => {
+  return (
+    <div className="HeaderLayoutContainer">
+      <img src={bluebackground} className="blueBackground" />
+      <h1 className="headerTiTle">IP Address Tracker</h1>
+      <div className="inputBox">
+        <input className="inputOfAddress" />
+        <button className="inputButton">
+          <img src={arrow} className="arrow" />
+        </button>
+      </div>
+      <div className="mapinfodisplaybox">
+        <div className="mapinfotextbox">
+          <a className="infotitle">IP Address</a>
+          <div className="mapinfo">192.212.174.101</div>
+        </div>
+        <div className="mapinfotextbox">
+          <a className="infotitle">IP Address</a>
+          <div className="mapinfo">192.212.174.101</div>
+        </div>
+        <div className="mapinfotextbox">
+          <a className="infotitle">IP Address</a>
+          <div className="mapinfo">192.212.174.101</div>
+        </div>
+        <div className="mapinfotextbox">
+          <a className="infotitle">IP Address</a>
+          <div className="mapinfo">192.212.174.101</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderLayout;
